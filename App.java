32c08162f270664f20b1d7c81d94aa5ec0bd52ff
samueldessbesell <samuel23270020@aluno.cesupa.br>
2025-10-04 21:12:28 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,17 +1,20 @@
-public class App {
+public class App{
     public static void main(String[] args) throws Exception {
         //Menu menu = new Menu();
         //menu.exibirMenu();
 
-        Jogador player1 = new Jogador("Pukeman", 15, 4, 2, 1, 3);
         Arma arma1 = new Arma("Quebra crânio", 2, "armaPesada");
+        Jogador player1 = new Jogador("Pukeman", 50, 4, 2, 1, 3, arma1, null, 10);
 
-        Adversario adversario1 = new Adversario("Vampiro", 25, 2, 5, 3, 4);
         Arma arma2 = new Arma("Foice ceifadora", 2, "armaLeve");
+        Adversario adversario1 = new Adversario("Vampiro", 40, 2, 5, 3, 4, arma2, null, 10);
 
-        do{
-            player1.atacar(adversario1, arma1);
-            adversario1.atacar(player1, arma2);
-        } while(player1.getHp() > 0 && adversario1.getHp() > 0);
+        Combate combate1 = new Combate(player1, adversario1);
+
+        player1.mostrarStatus();
+        adversario1.mostrarStatus();
+        System.out.println();
+
+        combate1.iniciarCombate(player1, adversario1);
     }
 }
