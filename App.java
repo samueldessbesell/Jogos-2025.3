4578e88422b13efa00c912cdbd84ed00ef1e7dfa
samueldessbesell <samuel23270020@aluno.cesupa.br>
2025-10-07 03:08:39 -0300

    Update App.java

--- App.java
+++ App.java
@@ -4,18 +4,14 @@ public class App{
         Jogo j = new Jogo();
         j.exibirMenu();
 
-        /*Arma arma1 = new Arma("Quebra crânio", 2, "armaPesada");
-        Jogador player1 = new Jogador("Pukeman", 50, 4, 2, 1, 3, arma1, null, 10);
-
-        Arma arma2 = new Arma("Foice ceifadora", 2, "armaLeve");
-        Adversario adversario1 = new Adversario("Vampiro", 40, 2, 5, 3, 4, arma2, null, 10);
-
+        /*
         Combate combate1 = new Combate(player1, adversario1);
 
         player1.mostrarStatus();
         adversario1.mostrarStatus();
         System.out.println();
 
-        combate1.iniciarCombate(player1, adversario1);*/
+        combate1.iniciarCombate(player1, adversario1);
+        */
     }
 }
